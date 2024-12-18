a51c50433518255f2b5e6d84c5cbebf5fa0272cc
verliere <verliere1999@gmail.com>
2024-12-18 17:14:29 +0400

    Add files via upload

---
+++ sdasdasd.py
@@ -0,0 +1,17 @@
+username = 'Лерой Дженкинс'
+print('Меня зовут:', username)
+
+title = 'Бугабуга'
+print('Заголовок заметки:', title)
+
+content = 'это лишнее'
+print('Описание заметки', content)
+
+status = 'Trye'
+print('Статус заметки:', status)
+
+created_date ='18-12-2024'
+print('Дата создания заметки:',created_date)
+
+issue_date ='18-12-2024'
+print('Дедлайн:',created_date)
